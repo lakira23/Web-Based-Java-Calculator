451e5bc5b8f1103f96db5b607b1318078d88f502
Lakira Weerasinghe <142226923+lakira23@users.noreply.github.com>
2025-11-09 23:32:24 +0000

    made a basic calculator engine

---
+++ calculator_engine.java
@@ -0,0 +1,20 @@
+
+public class calculator_engine {
+
+    public int add(int num1,int num2){
+        return num1 + num2;
+    }
+
+    public int substract(int num1,int num2){
+        return num1 - num2;
+    }
+
+    public double divide(int num1,int num2){
+        return num1 / num2;
+    }
+    
+    public int multiply(int num1, int num2){
+        return num1 * num2;
+    }
+}
+
